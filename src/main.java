bcab41ec47b9a6b7f07ef20aff4e8faaea3dda95
mjs9768 <mjs9768@uncw.edu>
2015-11-05 12:38:31 -0500

    4th commit labtop

---
+++ src/main.java
@@ -0,0 +1,28 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Hand testHand = new Hand();
+		Card card1 = new Card(14,2);
+		Card card2 = new Card(11,2);
+		Card card3 = new Card(13,2);
+		Card card4 = new Card(12,1);
+		Card card5 = new Card(10,3);
+		Card card6 = new Card(9,4);
+		testHand.addToHand(card1);
+		testHand.addToHand(card2);
+		testHand.addToHand(card3);
+		testHand.addToHand(card4);
+		testHand.addToHand(card5);
+		testHand.addToHand(card6);
+		HandEval eval = new HandEval(testHand);
+		System.out.println(card1);
+		System.out.println(card2);
+		System.out.println(card3);
+		System.out.println(card4);
+		System.out.println(card5);
+		System.out.println(eval.threeOfKindPercent());
+	}
+
+}
