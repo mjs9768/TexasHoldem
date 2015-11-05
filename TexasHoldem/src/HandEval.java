9163ebb4201bd1cc4b2784ea06d16bb3642df744
mjs9768 <mjs9768@uncw.edu>
2015-11-05 00:04:32 -0500

    3rd working on hand eval

--- TexasHoldem/src/HandEval.java
+++ TexasHoldem/src/HandEval.java
@@ -68,8 +68,8 @@ public class HandEval {
 		double percent = 0;
 		int i =0;
 		int z = 0;
+		int x = 0;
 		int missingCards = 0;
-		System.out.println("percent i got here" );
 		while(z<4){
 			while (i<currentHand.handSize()){ // putting in hand all of same suit
 				if (currentHand.get(i).getSuit() == z+1){
@@ -80,50 +80,127 @@ public class HandEval {
 					i++;
 				}
 			}
-			System.out.println("percent here to" );
 			goodCards.sortByRank();
-			if((goodCards.handSize() >= 5) &&
-					(goodCards.get(4).getRank() - goodCards.get(0).getRank() == 0)){
+			if((goodCards.handSize() >= 5) && (goodCards.get(4).getRank() - (goodCards.get(0).getRank()+4) == 0)){
 				percent = 1.0;
 				return percent;
 			}
-			if((goodCards.handSize() < (7 - currentHand.handSize()))){
-				percent = 0;
-			}
-			System.out.println("percent i got here 3 - " + goodCards.handSize());
-			while(i < goodCards.handSize()-1){
-				if (goodCards.get(i).getRank() == (goodCards.get(i+1).getRank() - 1)){
-					i++;
-					System.out.println("percent i got here 4");
+			while(x < (goodCards.handSize()-1)){
+				if (goodCards.get(x).getRank() == (goodCards.get(x+1).getRank() - 1)){
+					x++;
 				}
 				else{
-					System.out.println("percent i got here 5");
 					missingCards++;
-					i++;
+					x++;
 				}
-			}
-			if(missingCards >= 4){
-				System.out.println("percent i got here 3");
-				percent = 0.0;
-				return percent;
 			}
 			double cardsLeft = 52 - currentHand.handSize();
 			double percentage = 1;
 			while (missingCards > 0){	// calc odds to chance to get straight flush
-				System.out.println("percent " +  percentage);
 				percentage = percentage * (missingCards/cardsLeft);
+				System.out.println("percent " +  percentage);
 				missingCards--;
+				cardsLeft--;
+				percent = percent + percentage;
+			}
+			missingCards = 0;
+			x = 0;
+			i = 0;
+			System.out.println("percent here to -" + percent );
+			z++;
+			goodCards.clear();
+		}
+		return percent;
+	}	
+	public double fourKindPercent(){
+		currentHand.sortByRank();
+		int x = 0;
+		int i = 0;
+		int j = 0;
+		double percent = 0;
+		while (x < currentHand.handSize()- 3){ // if 4 cards are the same
+			if (currentHand.get(x).getRank() == currentHand.get(x+3).getRank()){
+				percent = 1;
+				return percent;
+			}
+			x++;
+		}
+		while (i < currentHand.handSize()- 2){ // if three cards
+			if (currentHand.get(i).getRank() == currentHand.get(i+2).getRank()){
+				percent = (1.0 / (52 - currentHand.handSize()));
+				return percent;
+			}
+			i++;
+		}
+		while (j < currentHand.handSize()- 1){ // if 2 cards
+			if (currentHand.get(j).getRank() == currentHand.get(j+1).getRank()){
+				percent = (1.0 / (52 - currentHand.handSize())) * (1.0 / (51 - currentHand.handSize()));
+				return percent;
+			}
+			j++;
+		}
+		return percent;
+	}
+	public double fullHousePercent(){
+	}
+	public double flushPercent(){
+		Hand goodCards = new Hand(); // we temp store all the cards that fit the description
+		double percent = 0;
+		int i =0;
+		int z = 0;
+		while(z<4){
+			while (i<currentHand.handSize()){ // putting in hand all of same suit
+				if (currentHand.get(i).getSuit() == z+1){
+					goodCards.addToHand(currentHand.get(i));
+					i++;
+				}
+				else{
+					i++;
+				}
+			}
+			if(goodCards.handSize() >= 5){
+				percent = 1;
+				return percent;
+			}
+			double percentage = 1;
+			double cardsNeeded =  5 - goodCards.handSize();
+			double cardsLeft = 13 - currentHand.handSize();
+			while (cardsNeeded > 0){	// calc odds to chance to get royal
+				percentage = percentage * (cardsNeeded/cardsLeft);
+				cardsNeeded--;
 				cardsLeft--;
 			}
 			i = 0;
 			percent = percent + percentage;
 			z++;
 			goodCards.clear();
+
 		}
-		if (percent < .000001) // removes impossible small scenarios 
-			return 0.0;
 		return percent;
-	}	
-	
+	}
+	public double straightPercent(){
+		currentHand.sortByRank();
+		int x = 0;
+		int i = 0;
+		int j = 0;
+		double percent = 0;
+		while (x < currentHand.handSize()- 4){
+			if ((currentHand.get(x+4).getRank() - currentHand.get(x).getRank()) == 4){
+				percent = 1;
+				return percent;
+			}
+			x++;
+		}
+		while (i < currentHand.handSize()- 4){
+			if (currentHand.handSize() == 5){
+				
+
+			}
+			i++;
+		}
+		
+		return percent;
+	}
 }
+
 
